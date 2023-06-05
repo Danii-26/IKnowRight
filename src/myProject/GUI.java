458494ef9a8bf604576b93ebbdefc72f14e38693
dniel <daniel.arias.castrillon@correounivalle.edu.co>
2023-06-05 18:23:09 -0500

    Ahora toma la lista de palabras desde un .txt c:

--- src/myProject/GUI.java
+++ src/myProject/GUI.java
@@ -1,5 +1,8 @@
 package myProject;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Vector;
@@ -14,11 +17,13 @@ public class GUI{
     private static final int tiempoDeJuego = 5000; //Tiempo de cada palabra
     private static final int tiempoDeRespuesta = 7000; //Tiempo de respuesta
 
-    private static final Vector palabras = new Vector(); //Vector de palabras
+    private static final List<String> palabras = new Vector<>(); //Vector de palabras
     private static final Random random = new Random();
     private static int nivel = 1; // Nivel actual
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args){
 
         listaPalabras();
 
@@ -26,20 +31,14 @@ public class GUI{
         System.out.print("Ingrese su nombre: ");
         String jugador = scanner.nextLine();
 
-        int nivelActual = getNivel(jugador);
-        if (nivelActual > 1) {
-            System.out.println(jugador + ", estás en el lvl." + nivelActual);
-            nivel = nivelActual;
-        } else {
-            System.out.println("Bienvenido " + jugador + "!");
-        }
+        System.out.println("Bienvenido " + jugador + "!");
+
 
         boolean continuar = true;
 
         while (continuar) {
             System.out.println("----lvl." + nivel + " ----");
             play();
-            actualizarNivel(nivel);
 
             System.out.print("Desea seguir jugando?(S/N): ");
             String playNextLevel = scanner.nextLine().toUpperCase();
@@ -55,28 +54,16 @@ public class GUI{
         scanner.close();
     }
 
-    private static void listaPalabras() {
-        palabras.add("java");
-        palabras.add("python");
-        palabras.add("listener");
-        palabras.add("gui");
-        palabras.add("scanner");
-        palabras.add("eventos");
-        palabras.add("programador");
-        palabras.add("hacker");
-    }
-    private static int getNivel(String playerName) {
-        // Lógica para obtener el nivel del jugador desde el almacenamiento (por ejemplo, una base de datos)
-        // Si no se encuentra el jugador, se asume que es nivel 1
-        // En este ejemplo, simplemente se devuelve 1
-        return 1;
-    }
-    private static void actualizarNivel(int level) {
-        // Lógica para actualizar el nivel del jugador en el almacenamiento (por ejemplo, una base de datos)
-        // En este ejemplo, no se hace nada ya que no se guarda ningún estado
+    private static void listaPalabras(){
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("palabras.txt"));
+            palabras.addAll(lines);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void play() {
+    private static void play(){
         Vector secuencia = crearSecuencia(nivel);
         System.out.println("Memoriza las siguientes palabras:");
         mostrarSecuencia(secuencia);
@@ -107,7 +94,7 @@ public class GUI{
         }
     }
 
-    private static Vector crearSecuencia(int level) {
+    private static Vector crearSecuencia(int level){
         Vector sequence = new Vector();
         for (int i = 0; i < level; i++) {
             int randomIndex = random.nextInt(palabras.size());
@@ -117,7 +104,7 @@ public class GUI{
         return sequence;
     }
 
-    private static void mostrarSecuencia(Vector<String> sequence) {
+    private static void mostrarSecuencia(Vector<String> sequence){
         for (String word : sequence) {
             System.out.println(word);
             tiempoLímite(tiempoDeJuego);
@@ -125,9 +112,9 @@ public class GUI{
         }
     }
 
-    private static Vector<String> crearListaPalabras(Vector<String> sequence) {
+    private static Vector<String> crearListaPalabras(Vector<String> sequence){
         Vector<String> wordListToGuess = new Vector<>(sequence);
-        for (String word : sequence) {
+        for (String word : sequence){
             int randomIndex = random.nextInt(palabras.size());
             String randomWord = (String) palabras.get(randomIndex);
             wordListToGuess.add(randomWord);
@@ -135,13 +122,13 @@ public class GUI{
         return wordListToGuess;
     }
 
-    private static void mostrarLista(Vector<String> wordList) {
-        for (String word : wordList) {
+    private static void mostrarLista(Vector<String> wordList){
+        for (String word : wordList){
             System.out.println(word);
         }
     }
 
-    private static String getTiempoUsuario(int timeLimit) {
+    private static String getTiempoUsuario(int timeLimit){
         Scanner scanner = new Scanner(System.in);
         final String[] userInput = {""};
         Thread inputThread = new Thread(() -> {
@@ -151,7 +138,7 @@ public class GUI{
         inputThread.start();
         try {
             Thread.sleep(timeLimit);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException e){
             e.printStackTrace();
         }
 
@@ -159,15 +146,15 @@ public class GUI{
         return userInput[0];
     }
 
-    private static void tiempoLímite(int milliseconds) {
+    private static void tiempoLímite(int milliseconds){
         try {
             Thread.sleep(milliseconds);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException e){
             e.printStackTrace();
         }
     }
 
-    private static void clearConsole() {
+    private static void clearConsole(){
         try {
             final String os = System.getProperty("os.name");
             if (os.contains("Windows")) {
@@ -177,7 +164,7 @@ public class GUI{
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
             }
-        } catch (final Exception e) {
+        } catch (final Exception e){
             e.printStackTrace();
         }
     }
